b2a5345f97812ef28b6d3ed0550cf3c8dcb323a6
xiaofang <1692977129@qq.com>
2018-10-17 14:46:29 +0800

    table switch chart

--- src/app/super/components/table-switch-chart.component.ts
+++ src/app/super/components/table-switch-chart.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-table-switch-chart',
-  templateUrl: './table-switch-chart.component.html',
-  styles: []
+    selector: 'app-table-switch-chart',
+    templateUrl: './table-switch-chart.component.html',
+    styles: []
 })
 export class TableSwitchChartComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    dataSet: any[] = [];
+    ngOnInit() {
+        for (var i = 0; i < 50; i++) {
+
+            this.dataSet.push(
+                {
+                    name: "xf"+i,
+                    age: i+1,
+                    address: "addr"+i
+                }
+            );
+
+
+        }
+    }
 
 }
