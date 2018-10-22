0f5d7bfefdae5951f29e2af3b64253f4561ba128
xiaofang <1692977129@qq.com>
2018-10-22 15:26:40 +0800

    add net

---
+++ src/app/pages/mrna/net.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: 'app-net',
+    templateUrl: './net.component.html'
+})
+
+export class netComponent implements OnInit {
+    ngOnInit() {
+
+    }
+}
