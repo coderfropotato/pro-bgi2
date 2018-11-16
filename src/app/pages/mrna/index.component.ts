45902d6ccaec9cf6a1ca031ecd2300e793499ef9
yangwd <yangwd@gooalgene.com>
2018-11-16 15:07:41 +0800

    Update

--- src/app/pages/mrna/index.component.ts
+++ src/app/pages/mrna/index.component.ts
@@ -47,7 +47,7 @@ export class IndexComponent implements OnInit {
         (async () => {
             try {
                 // await this.getLcInfo();
-                // await this.getAddThead();
+                await this.getAddThead();
                 await this.getMenuList();
                 this.ready = true;
                 setTimeout(() => {
--- src/app/super/service/toolsService.ts
+++ src/app/super/service/toolsService.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 /**
- * @description 小工具服务
+ * @description 小工具组件服务
  * @author Yangwd<277637411@qq.com>
  * @date 2018-11-13 10:47:55
  * @export
@@ -12,24 +12,25 @@ import { Injectable } from "@angular/core";
 export class ToolsService {
     visible:boolean = false;
     geneCount:number = 0;
-    geneList = [];
 
     constructor() {}
 
     init(){
         this.geneCount = 0;
-        this.geneList = [];
     }
 
-    showTools(geneList){
+    showTools(total,tableEntity){
+        if(tableEntity['others']['checkStatus']){
+            this.geneCount = total - tableEntity['others']['excludeGeneList']['unChecked'].length;
+        }else{
+            this.geneCount = tableEntity['others']['excludeGeneList']['checked'].length;
+        }
         this.visible = true;
-        this.geneList = geneList;
-        this.geneCount = geneList.length;
     }
 
     hide(){
-        this.visible = false;
         this.init();
+        this.visible = false;
     }
 
 }
