244253fddadeb88f0aa3174470142b69b4c97ade
dongchuangchuang <dongcc@gooalgene.com>
2018-11-27 20:54:14 +0800

    venn

--- src/app/pages/mrna/upload.component.ts
+++ src/app/pages/mrna/upload.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NzModalService,UploadFile } from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-upload',
@@ -6,8 +7,99 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class UploadComponent implements OnInit {
-  constructor() { }
+
+  tabs: object;
+  resultList:object;
+  isShowTab: boolean = true;
+  isVisible: boolean = false;
+  fileList: UploadFile[] = [];
+  fileList2: UploadFile[] = [];
+  fileList3: UploadFile[] = [];
+  index:number=0;
+  tab1:object;
+
+  constructor(private modalService: NzModalService) {}
 
   ngOnInit() {
+    this.tabs = [
+      {
+        name : '基因信息',
+        disabled: false
+      },
+      {
+        name : '转录本信息',
+        disabled: false
+      },
+      {
+        name : '样本信息',
+        disabled: false
+      }
+    ];
+
+    this.tab1={
+
+    }
+      
   }
+
+
+  beforeUpload = (file: UploadFile): boolean => {
+    this.fileList.push(file);
+    console.log(this.fileList)
+    
+    return false;
+  }
+  beforeUpload2 = (file: UploadFile): boolean => {
+    this.fileList2.push(file);
+    console.log(this.fileList2[0]['name'])
+    return false;
+  }
+  beforeUpload3 = (file: UploadFile): boolean => {
+    this.fileList3.push(file);
+    console.log(this.fileList3[0]['name'])
+    return false;
+  }
+
+  fileListChange(){
+
+  }
+
+  SelectedIndexChange(num){
+    this.index=num;
+    //console.log(num)
+  }
+
+
+  goResult() {
+    let temp = '';
+    if(this.index==0){
+      let t_name = '';
+      this.fileList.forEach(element => {
+        t_name += element.name+"/";
+      });
+      temp=t_name+"上传成功!!!"
+    }else if(this.index==1){
+      let t_name = '';
+      this.fileList2.forEach(element => {
+        t_name += element.name+"/";
+      });
+      temp=t_name+"上传成功!!!"
+    }else if(this.index==2){
+      let t_name = '';
+      this.fileList3.forEach(element => {
+        t_name += element.name+"/";
+      });
+      temp=t_name+"上传成功!!!"
+    }
+    this.modalService.success({
+      nzTitle: '上传结果',
+      nzContent: temp
+    });
+    this.isShowTab = true;
+  }
+
+  updateLoad(){
+    this.isShowTab = false;
+  }
+
 }
