d75f988b9073bf4cc3e0bbfa6fbc662f0235e1a6
yangwd <yangwd@gooalgene.com>
2019-01-21 14:18:06 +0800

    Update

--- src/app/pages/reanalysis/index.component.ts
+++ src/app/pages/reanalysis/index.component.ts
@@ -73,52 +73,52 @@ export class ReanalysisIndexComponent implements OnInit {
         });
     }
 
-    async getAddThead() {
-        return new Promise((resolve, reject) => {
-            let LCID = sessionStorage.getItem("LCID");
-            this.ajaxService
-                .getDeferData({
-                    data: {},
-                    url: `${config['javaPath']}/addColumn/${LCID}`
-                })
-                .subscribe(
-                    data => {
-                        if(data['status']==='0'){
-                            let d = data['data'];
-                            // let outerFlag = "006";
-                            // let outerDataBase;
+    // async getAddThead() {
+    //     return new Promise((resolve, reject) => {
+    //         let LCID = sessionStorage.getItem("LCID");
+    //         this.ajaxService
+    //             .getDeferData({
+    //                 data: {},
+    //                 url: `${config['javaPath']}/addColumn/${LCID}`
+    //             })
+    //             .subscribe(
+    //                 data => {
+    //                     if(data['status']==='0'){
+    //                         let d = data['data'];
+    //                         // let outerFlag = "006";
+    //                         // let outerDataBase;
 
-                            d.forEach((val,index)=>{
-                                if(val['category']===config['outerDataBaseIndex']){
-                                    // outerDataBase = d.splice(index,1)[0];
-                                    val['children'].forEach(v=>{
-                                        if(!('children' in v)) v['children'] = [];
-                                        v['modalVisible'] = false;
-                                        v['children'].forEach(item=>{
-                                            this.initTreeData(item['treeData']);
-                                        })
-                                    })
-                                }
-                            })
+    //                         d.forEach((val,index)=>{
+    //                             if(val['category']===config['outerDataBaseIndex']){
+    //                                 // outerDataBase = d.splice(index,1)[0];
+    //                                 val['children'].forEach(v=>{
+    //                                     if(!('children' in v)) v['children'] = [];
+    //                                     v['modalVisible'] = false;
+    //                                     v['children'].forEach(item=>{
+    //                                         this.initTreeData(item['treeData']);
+    //                                     })
+    //                                 })
+    //                             }
+    //                         })
 
-                            // this.storeService.setThead(d);
-                            this.addColumnService.set(d);
-                            // outerDataBase['children'].forEach(v=>{
-                            //     v['children'].forEach((val,index)=>{
-                            //         val['generatedThead'] = [];
-                            //         this.initTreeData(val['treeData']);
-                            //     })
-                            // });
-                            // this.outerDataBaseService.set(outerDataBase);
-                            resolve("success");
-                        }else{
-                            reject('error');
-                        }
-                    },
-                    () => reject("error")
-                );
-        });
-    }
+    //                         // this.storeService.setThead(d);
+    //                         this.addColumnService.set(d);
+    //                         // outerDataBase['children'].forEach(v=>{
+    //                         //     v['children'].forEach((val,index)=>{
+    //                         //         val['generatedThead'] = [];
+    //                         //         this.initTreeData(val['treeData']);
+    //                         //     })
+    //                         // });
+    //                         // this.outerDataBaseService.set(outerDataBase);
+    //                         resolve("success");
+    //                     }else{
+    //                         reject('error');
+    //                     }
+    //                 },
+    //                 () => reject("error")
+    //             );
+    //     });
+    // }
 
     // 初始化 增删列树节点数据
     initTreeData(treeData){
--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,9 @@ const config: object = {
     javaPath:"http://47.107.224.133/api", // api基础路径
     outerDataBaseIndex:"006", // 增删列外部数据库的索引
     lang:"zh",  // 默认语言
+    geneTypeAll:"all",  // 基因和转录本切换 基因类型为all表示有基因切换 默认为gene
+    geneTypeOfGene:'gene', // 基因和转录本切换 基因类型为gene的字段名称
+    geneTypeOfTranscript:"transcript",   // 基因和转录本切换 基因类型为transcript的字段名称
     sysDefend:false // 是否系统维护
 };
 
