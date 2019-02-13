2a62d71207f80590d0b20482d83111990079942a
yangwd <yangwd@gooalgene.com>
2019-02-13 16:05:51 +0800

    Rebuild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ export class AppComponent {
     constructor(private message: MessageService) {}
 
     ngOnInit() {
-        console.log("%cRebuild-Success:2019-01-22 10:24"," color:green;;font-size:1.0em");
+        console.log("%cRebuild-Success:2019-02-13 16:02"," color:green;;font-size:1.0em");
         fromEvent(window, "resize")
             .pipe(debounceTime(300))
             .subscribe(event => {
