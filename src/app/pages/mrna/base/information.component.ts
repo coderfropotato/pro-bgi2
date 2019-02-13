d4523d61472242b399023d057d5023e626f569d5
yangwd <yangwd@gooalgene.com>
2019-02-13 14:42:43 +0800

    Update : format

--- src/app/pages/mrna/base/information.component.ts
+++ src/app/pages/mrna/base/information.component.ts
@@ -17,177 +17,175 @@ declare const $: any;
 	styles: []
 })
 export class InformationComponent implements OnInit {
-  @ViewChild('RNAClassChart') RNAClassChart;
-  @ViewChild('RNALchart') RNALchart;
-  @ViewChild('miRNAFchart') miRNAFchart;
-  @ViewChild('transcriptLength') transcriptLength;
-  @ViewChild('exonsNum') exonsNum;
+	@ViewChild('RNAClassChart') RNAClassChart;
+	@ViewChild('RNALchart') RNALchart;
+	@ViewChild('miRNAFchart') miRNAFchart;
+	@ViewChild('transcriptLength') transcriptLength;
+	@ViewChild('exonsNum') exonsNum;
 
-  //2.2 RNA分类
-  tableUrl:string;
-  tableEntity:object;
-  chart:any;
+	//2.2 RNA分类
+	tableUrl: string;
+	tableEntity: object;
+	chart: any;
 
-  isShowColorPanel: boolean = false;
-  legendIndex: number = 0; //当前点击图例的索引
-  color: string; //当前选中的color
+	isShowColorPanel: boolean = false;
+	legendIndex: number = 0; //当前点击图例的索引
+	color: string; //当前选中的color
 
-  //2.3 基因长度
-  transcriptLengthUrl:string;
-  transcriptLengthEntity:object;
-  chartT:any;
-  selectPanelData: object[] = [];
-  selectConfirmData: string[] = [];
+	//2.3 基因长度
+	transcriptLengthUrl: string;
+	transcriptLengthEntity: object;
+	chartT: any;
+	selectPanelData: object[] = [];
+	selectConfirmData: string[] = [];
 
-  isShowColorPanelT: boolean = false;
-  legendIndexT: number = 0; //当前点击图例的索引
-  colorT: string; //当前选中的color
+	isShowColorPanelT: boolean = false;
+	legendIndexT: number = 0; //当前点击图例的索引
+	colorT: string; //当前选中的color
 
-  //2.4 miRNA长度分布
-  tableRNAUrl:string;
-  tableRNAEntity:object;
-  chartRNA:any;
+	//2.4 miRNA长度分布
+	tableRNAUrl: string;
+	tableRNAEntity: object;
+	chartRNA: any;
 
-  isShowColorPanelL: boolean = false;
-  legendIndexL: number = 0; //当前点击图例的索引
-  colorL: string; //当前选中的color
+	isShowColorPanelL: boolean = false;
+	legendIndexL: number = 0; //当前点击图例的索引
+	colorL: string; //当前选中的color
 
-  //2.5 外显子数量
-  exonsNumUrl:string;
-  exonsNumEntity:object;
-  chartE:any;
-  selectPanelDataE: object[] = [];
-  selectConfirmDataE: string[] = [];
+	//2.5 外显子数量
+	exonsNumUrl: string;
+	exonsNumEntity: object;
+	chartE: any;
+	selectPanelDataE: object[] = [];
+	selectConfirmDataE: string[] = [];
 
-  isShowColorPanelE: boolean = false;
-  legendIndexE: number = 0; //当前点击图例的索引
-  colorE: string; //当前选中的color
+	isShowColorPanelE: boolean = false;
+	legendIndexE: number = 0; //当前点击图例的索引
+	colorE: string; //当前选中的color
 
-  // table one 2.6
-  defaultEntity: object;
-  defaultUrl: string;
-  defaultTableId: string;
+	// table one 2.6
+	defaultEntity: object;
+	defaultUrl: string;
+	defaultTableId: string;
 
-  //2.7 miRNA首位碱基分布
-  tableRNAFUrl:string;
-  tableRNAFEntity:object;
-  chartFRNA:any;
+	//2.7 miRNA首位碱基分布
+	tableRNAFUrl: string;
+	tableRNAFEntity: object;
+	chartFRNA: any;
 
-  isShowColorPanelF: boolean = false;
-  legendIndexF: number = 0; //当前点击图例的索引
-  colorF: string; //当前选中的color
+	isShowColorPanelF: boolean = false;
+	legendIndexF: number = 0; //当前点击图例的索引
+	colorF: string; //当前选中的color
 
+	constructor(
+		private message: MessageService,
+		private store: StoreService,
+		private ajaxService: AjaxService,
+		private globalService: GlobalService,
+		private storeService: StoreService,
+		private promptService: PromptService,
+		private router: Router
+	) {}
 
-  constructor(
-    private message: MessageService,
-    private store: StoreService,
-    private ajaxService: AjaxService,
-    private globalService: GlobalService,
-    private storeService: StoreService,
-    private promptService:PromptService,
-    private router: Router
-  ) { }
+	ngOnInit() {
+		//2.2 RNA分类
 
-  ngOnInit() {
+		this.tableUrl = `${config['javaPath']}/basicModule/RNAClass`;
+		this.tableEntity = {
+			LCID: this.store.getStore('LCID')
+		};
 
-    //2.2 RNA分类
+		//2.3 基因长度
+		let m_geneTypeList = [ 'mrna|known', 'mrna|novel', 'lncrna|known', 'lncrna|novel' ];
 
-    this.tableUrl=`${config["javaPath"]}/basicModule/RNAClass`;
-    this.tableEntity={
-      LCID: this.store.getStore('LCID')
-    };
+		this.selectPanelData = [
+			{
+				type: '',
+				data: m_geneTypeList
+			}
+		];
 
-    //2.3 基因长度
-    let m_geneTypeList = ['mrna|known','mrna|novel','lncrna|known','lncrna|novel'];
+		this.transcriptLengthUrl = `${config['javaPath']}/basicModule/transcriptLength`;
+		this.transcriptLengthEntity = {
+			LCID: this.store.getStore('LCID'),
+			geneTypeList: m_geneTypeList
+		};
 
-    this.selectPanelData = [
-      {
-        type: '',
-        data: m_geneTypeList
-      }
-    ]
+		//2.4 miRNA长度分布
+		this.tableRNAUrl = `${config['javaPath']}/basicModule/miRNALength`;
+		this.tableRNAEntity = {
+			LCID: this.store.getStore('LCID')
+		};
 
-    this.transcriptLengthUrl=`${config["javaPath"]}/basicModule/transcriptLength`;
-    this.transcriptLengthEntity={
-      LCID: this.store.getStore('LCID'),
-      geneTypeList: m_geneTypeList
-    };
+		//2.5 外显子数量
 
-    //2.4 miRNA长度分布
-    this.tableRNAUrl=`${config["javaPath"]}/basicModule/miRNALength`;
-    this.tableRNAEntity={
-      LCID: this.store.getStore('LCID'),
-    };
+		this.selectPanelDataE = [
+			{
+				type: '',
+				data: m_geneTypeList
+			}
+		];
 
-    //2.5 外显子数量
+		this.exonsNumUrl = `${config['javaPath']}/basicModule/exonsNum`;
+		this.exonsNumEntity = {
+			LCID: this.store.getStore('LCID'),
+			RefGeneTypeList: m_geneTypeList
+		};
 
-    this.selectPanelDataE = [
-      {
-        type: '',
-        data: m_geneTypeList
-      }
-    ]
+		//2.6 小RNA数量
+		this.defaultUrl = `${config['javaPath']}/basicModule/annotationStat`;
+		this.defaultEntity = {
+			LCID: this.store.getStore('LCID'),
+			pageNum: 1,
+			pageSize: 10
+		};
 
-    this.exonsNumUrl=`${config["javaPath"]}/basicModule/exonsNum`;
-    this.exonsNumEntity={
-      LCID: this.store.getStore('LCID'),
-      RefGeneTypeList: m_geneTypeList
-    };
+		//2.7 miRNA首位碱基分布
+		this.tableRNAFUrl = `${config['javaPath']}/basicModule/miRNAFirstBaseBias`;
+		this.tableRNAFEntity = {
+			LCID: this.store.getStore('LCID')
+		};
+	}
 
-    //2.6 小RNA数量
-    this.defaultUrl = `${config["javaPath"]}/basicModule/annotationStat`;
-    this.defaultEntity = {
-        LCID: this.store.getStore('LCID'),
-        pageNum: 1,
-        pageSize: 10
-    };
+	//2.2 RNA分类
+	drawRNAClass(data) {
+		let temp = data.rows[0];
+		let tempArray = [
+			{
+				name: 'lncrna_num',
+				value: temp.lncrna_num
+			},
+			{
+				name: 'mirna_num',
+				value: temp.mirna_num
+			},
+			{
+				name: 'mrna_num',
+				value: temp.mrna_num
+			}
+		];
 
-    //2.7 miRNA首位碱基分布
-    this.tableRNAFUrl=`${config["javaPath"]}/basicModule/miRNAFirstBaseBias`;
-    this.tableRNAFEntity={
-      LCID: this.store.getStore('LCID')
-    };
-  }
+		let that = this;
 
-  //2.2 RNA分类
-  drawRNAClass(data){
-    let temp = data.rows[0];
-    let tempArray = [
-      {
-        name:"lncrna_num",
-        value:temp.lncrna_num
-      },
-      {
-        name:"mirna_num",
-        value:temp.mirna_num
-      },
-      {
-        name:"mrna_num",
-        value:temp.mrna_num
-      }
-    ];
-
-    let that = this;
-
-    let config:object={
-      chart: {
-				title: "各类型RNA数量统计",
-        dblclick: function(event,title) {
-          let text = title.firstChild.nodeValue;
-          that.promptService.open(text,(data)=>{
-              title.textContent = data;
-          })
-        },
-        width:600,
-        height:400,
-				padding:0.01,
-				outerRadius:120,
-				startAngle:0,
-				endAngle:360,
-				showLabel:true,
-				custom: ["name", "value"],
-				el: "#RNAClassID",
-				type: "pie",
+		let config: object = {
+			chart: {
+				title: '各类型RNA数量统计',
+				dblclick: function(event, title) {
+					let text = title.firstChild.nodeValue;
+					that.promptService.open(text, (data) => {
+						title.textContent = data;
+					});
+				},
+				width: 600,
+				height: 400,
+				padding: 0.01,
+				outerRadius: 120,
+				startAngle: 0,
+				endAngle: 360,
+				showLabel: true,
+				custom: [ 'name', 'value' ],
+				el: '#RNAClassID',
+				type: 'pie',
 				data: tempArray
 			},
 			legend: {
@@ -204,7 +202,7 @@ export class InformationComponent implements OnInit {
 			}
 		};
 
-        this.chart = new d4().init(config);
+		this.chart = new d4().init(config);
 	}
 
 	//2.3基因长度
@@ -290,13 +288,13 @@ export class InformationComponent implements OnInit {
 		var chartData = [];
 		for (var i = 0; i < baseThead.length; i++) {
 			for (var j = 0; j < rows.length; j++) {
-        if(baseThead[i].name != "mirna_length"){
-          chartData.push({
-            key: rows[j].mirna_length,
-            value: rows[j][baseThead[i].true_key],
-            category: baseThead[i].name
-          });
-        }
+				if (baseThead[i].name != 'mirna_length') {
+					chartData.push({
+						key: rows[j].mirna_length,
+						value: rows[j][baseThead[i].true_key],
+						category: baseThead[i].name
+					});
+				}
 			}
 		}
 
@@ -304,7 +302,7 @@ export class InformationComponent implements OnInit {
 
 		let config: object = {
 			chart: {
-				title: "miRNA长度分布",
+				title: 'miRNA长度分布',
 				dblclick: function(event) {
 					var name = prompt('请输入需要修改的标题', '');
 					if (name) {
@@ -313,9 +311,9 @@ export class InformationComponent implements OnInit {
 					}
 				},
 				el: '#RNALData',
-                type: 'groupBar',
-                innerPadding:0.01,
-                width: 800,
+				type: 'groupBar',
+				innerPadding: 0.01,
+				width: 800,
 				custom: [ 'key', 'value', 'category' ],
 				data: chartData
 			},
@@ -328,7 +326,7 @@ export class InformationComponent implements OnInit {
 							this.setXTitle(name);
 							this.updateTitle();
 						}
-					},
+					}
 					//rotate: 60
 				},
 				y: {
@@ -339,7 +337,7 @@ export class InformationComponent implements OnInit {
 							this.setYTitle(name);
 							this.updateTitle();
 						}
-					},
+					}
 					//formatter: (val) => val + '%'
 				}
 			},
@@ -363,40 +361,39 @@ export class InformationComponent implements OnInit {
 					'</span>'
 				);
 			}
-    }
+		};
 
-    this.chartRNA = new d4().init(config);
+		this.chartRNA = new d4().init(config);
+	}
 
-  }
+	//2.5 外显子数量
+	drawExonsNum(data) {
+		console.log(data);
+		var baseThead = data.baseThead;
+		var rows = data.rows;
+		var chartData = [];
 
-  //2.5 外显子数量
-  drawExonsNum(data){
-    console.log(data);
-    var baseThead = data.baseThead;
-    var rows = data.rows;
-    var chartData = [];
+		for (var i = 0; i < rows.length; i++) {
+			let temp = {};
+			let total = 0;
+			for (let j = 0; j < baseThead.length; j++) {
+				let tempName = baseThead[j].true_key;
+				let tempValue = rows[i][tempName];
+				temp[tempName] = tempValue;
+				// if(tempName != "ref_all"&&tempName !="ref_item"){
+				//   total += tempValue;
+				// }
+			}
+			temp['total'] = total;
+			chartData.push(temp);
+		}
+		console.log(chartData);
 
-    for (var i = 0; i < rows.length; i++) {
-      let temp = {};
-      let total = 0;
-      for (let j = 0; j < baseThead.length; j++) {
-        let tempName = baseThead[j].true_key;
-        let tempValue = rows[i][tempName];
-        temp[tempName] = tempValue;
-        // if(tempName != "ref_all"&&tempName !="ref_item"){
-        //   total += tempValue;
-        // }
-      }
-      temp["total"] = total;
-      chartData.push(temp)
-    }
-    console.log(chartData);
+		let that = this;
 
-    let that = this;
-
-    let config:object={
-      chart: {
-				title: "外显子数量分布图",
+		let config: object = {
+			chart: {
+				title: '外显子数量分布图',
 				dblclick: function(event) {
 					var name = prompt('请输入需要修改的标题', '');
 					if (name) {
@@ -419,7 +416,7 @@ export class InformationComponent implements OnInit {
 							this.setXTitle(name);
 							this.updateTitle();
 						}
-					},
+					}
 					// rotate:60
 				},
 				y: {
