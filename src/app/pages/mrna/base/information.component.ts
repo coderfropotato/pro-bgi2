83b0d61e380a986c69d12742465df9b7d4ef11e5
dongchuangchuang <dongcc@gooalgene.com>
2019-03-07 17:40:43 +0800

    基础模块

--- src/app/pages/mrna/base/information.component.ts
+++ src/app/pages/mrna/base/information.component.ts
@@ -198,7 +198,7 @@ export class InformationComponent implements OnInit {
 				outerRadius: 120,
 				startAngle: 0,
 				endAngle: 360,
-				showLabel: true,
+				showLabel: false,
 				custom: [ 'name', 'value' ],
 				el: '#RNAClassID',
 				type: 'pie',
