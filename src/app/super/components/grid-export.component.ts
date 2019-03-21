e39e81c82a079f9ea153ddb8f0b0244aa7aca1f0
yangwd <yangwd@gooalgene.com>
2019-03-21 15:33:53 +0800

    Update : 菜单修改

--- src/app/super/components/grid-export.component.ts
+++ src/app/super/components/grid-export.component.ts
@@ -1,40 +1,43 @@
-import { AjaxService } from "./../service/ajaxService";
-import { Component, OnInit, Input } from "@angular/core";
-declare const $:any;
+import { LoadingService } from './../service/loadingService';
+import { AjaxService } from './../service/ajaxService';
+import { Component, OnInit, Input } from '@angular/core';
+declare const $: any;
 @Component({
-    selector: "app-grid-export",
-    template: `<i class="iconfont icon-xiazai" [nzTitle]="'tableButton.download' | translate" nz-tooltip (click)="download()"></i>`,
-    styles: []
+	selector: 'app-grid-export',
+	template: `<i class="iconfont icon-xiazai" [nzTitle]="'tableButton.download' | translate" nz-tooltip (click)="download()"></i>`,
+	styles: []
 })
 export class GridExportComponent implements OnInit {
-    // 查询表格的api
-    @Input() url: string;
-    // 表格的查询参数
-    @Input() tableEntity: object;
-    // 表格的下载名称
-    @Input() fileName: any;
-    
+	// 查询表格的api
+	@Input() url: string;
+	// 表格的查询参数
+	@Input() tableEntity: object;
+	// 表格的下载名称
+	@Input() fileName: any;
 
-    constructor(private ajaxService: AjaxService) {}
+	constructor(private ajaxService: AjaxService,private loadingService:LoadingService) {}
 
-    ngOnInit() {}
+	ngOnInit() {}
 
+	download() {
+		let entity = $.extend(true, {}, this.tableEntity);
+		entity['isExport'] = true;
+		entity['fileName'] = this.fileName || new Date().getTime();
 
-    download() {
-        let entity = $.extend(true,{},this.tableEntity);
-        entity["isExport"] = true;
-        entity["fileName"] = this.fileName || new Date().getTime();
-
-        this.ajaxService
-            .getDeferData({
-                url: this.url,
-                data: entity
-            })
-            .subscribe(
-                data => {},
-                error => {
-                    console.log(error);
-                }
-            );
-    }
+        this.loadingService.open('body','正在导出表格，请稍后...')
+        setTimeout(()=>{
+            this.loadingService.close();
+        },2000)
+		this.ajaxService
+			.getDeferData({
+				url: this.url,
+				data: entity
+			})
+			.subscribe(
+				(data) => {},
+				(error) => {
+					console.log(error);
+				}
+			);
+	}
 }
