2dad7d0bfa83dff3796b74ec8130ce98142045cb
dongchuangchuang <dongcc@gooalgene.com>
2019-04-26 15:46:57 +0800

    更新

--- src/app/pages/mrna/base/lrna.component.ts
+++ src/app/pages/mrna/base/lrna.component.ts
@@ -197,19 +197,6 @@ export class LrnaComponent implements OnInit {
 		let tempWidth = 0;
 
 		tempWidth = chartData.length *18;
-		// if(xData.length<=4){
-		// 	tempWidth = 900;
-		// }else if(xData.length>4 && xData.length <= 8){
-		// 	tempWidth = 1200;
-		// }else if(xData.length>8 && xData.length <= 12){
-		// 	tempWidth = 1400;
-		// }else if(xData.length>12 && xData.length <= 16){
-		// 	tempWidth = 1600;
-		// }else if(xData.length>16 && xData.length <= 20){
-		// 	tempWidth = 1800;
-		// }else{
-		// 	tempWidth = 2000;
-		// }
 
 		let config: object = {
 			chart: {
