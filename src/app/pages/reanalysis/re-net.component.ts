cffa0c81f27f3eb4430a5d9b2b445d4152a02d73
xiaofang <xiaofang@gooalgene.com>
2019-05-09 17:10:52 +0800

    net arrow

--- src/app/pages/reanalysis/re-net.component.ts
+++ src/app/pages/reanalysis/re-net.component.ts
@@ -413,6 +413,7 @@ export class ReNetComponent implements OnInit {
         let nodes=data.nodes,links=data.links;
         let values=data.value;
         let min=values[0],max=values[1];
+        let targetLinks=[];
 
         nodes.forEach(d => {
             if(!d.type){
@@ -454,9 +455,12 @@ export class ReNetComponent implements OnInit {
                     d.scale=m.scale;
                 }
             })
+
+            if(d.type==='target'){
+                targetLinks.push(d);
+            }
         })
 
-        // let arrows = [{ id: 'end-arrow', opacity: 1 }, { id: 'end-arrow-fade', opacity: 0.1 }]; //箭头
 
         this.allNodes=[...nodes];
         // add link
@@ -513,6 +517,23 @@ export class ReNetComponent implements OnInit {
             .domain(typeArr)
             .range(shapeArr);
 
+        //力图
+        let simulation = d3.forceSimulation()
+            .force("link", d3.forceLink().id(d=> d.geneID).iterations(4))
+            .force('charge', d3.forceManyBody().strength(-this.chartEntity['force']))
+            // .force("collide", d3.forceCollide().radius(d =>Math.sqrt(sizeScale(d.value))))  // 添加碰撞检测，使节点不重叠
+            .force('center', d3.forceCenter(width/2, height/2))
+            .force("x", d3.forceX())
+            .force("y", d3.forceY())
+
+        simulation
+            .nodes(nodes)
+            .on('tick', ticked);
+
+        simulation
+            .force('link')
+            .links(links);
+
         //图例svg
         let legendSvg=d3.select("#netChartDiv").append('svg').attr('id',"legendSvg");
 
@@ -527,33 +548,23 @@ export class ReNetComponent implements OnInit {
                     })
             )
             .on("dblclick.zoom", null);
-
+            
         //箭头
         svg.append("defs")
-            // .selectAll("marker")
-            // .data(arrows).enter()
+            .selectAll("marker")
+            .data(targetLinks).enter()
             .append("marker")
-            .attr("id", "end-arrow")
+            .attr("id", d=>'arrow'+d.id)
             .attr("viewBox", '0 0 20 20')
-            .attr("refX", 0)
+            .attr("refX", d=>Math.sqrt(sizeScale(d.target.value))+14)
             .attr("refY", 5)
             .attr("markerWidth", 4)
             .attr("markerHeight", 4)
             .attr("orient", "auto")
             .append("path")
             .attr("d", 'M0,0 L0,10 L10,5 z')
-            // .attr("opacity", d => d.opacity);
 
         let g = svg.append("g");
-
-        //力图
-        let simulation = d3.forceSimulation()
-            .force("link", d3.forceLink().id(d=> d.geneID).iterations(4))
-            .force('charge', d3.forceManyBody().strength(-this.chartEntity['force']))
-            // .force("collide", d3.forceCollide().radius(d => sizeScale(d.value)))  // 添加碰撞检测，使节点不重叠
-            .force('center', d3.forceCenter(width/2, height/2))
-            .force("x", d3.forceX())
-            .force("y", d3.forceY())
 
         //link
         let link = g.append('g')
@@ -567,7 +578,7 @@ export class ReNetComponent implements OnInit {
             .attr('stroke-width', 2)
             .attr("fill", "none")
             .style('cursor','pointer')
-            .attr("marker-end",d=> d.type==='target' ? 'url(#end-arrow)' :'')
+            .attr("marker-end",d=> d.type==='target' ? `url(#arrow${d.id})` :'')
             .on("mouseover", m => {
                 let referencesStr= m.references;
                 let references=[];
@@ -701,13 +712,6 @@ export class ReNetComponent implements OnInit {
             .on('drag', dragged)
             .on('end', dragended))
 
-        simulation
-            .nodes(nodes)
-            .on('tick', ticked);
-
-        simulation
-            .force('link')
-            .links(links);
 
         //node text
         if(that.chartEntity['symbolType'] !=='hidden'){
